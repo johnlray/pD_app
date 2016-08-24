5171c207778b1a5493acc7b37b20b9e0e0451612
John Ray <j.logan.ray@gmail.com>
2016-08-23 20:45:58 -0700

    code to load libraries

--- server.R
+++ server.R
@@ -1,5 +1,11 @@
 # server.R
 
+list.of.packages <- c("ggplot2", "lattice","plotly","shiny","shinythemes")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+rm(list.of.packages)
+rm(new.packages)
+
 library(ggplot2)
 library(lattice)
 library(plotly)
--- ui.R
+++ ui.R
@@ -1,5 +1,11 @@
 # ui.R
 
+list.of.packages <- c("ggplot2", "lattice","plotly","shiny","shinythemes")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+rm(list.of.packages)
+rm(new.packages)
+
 newpal <- c('#19969F', '#B5522E', '#7E8DCF', '#FCFDFC', '#E38E52', '#ADE4F6', '#121D9E', '#06091D', '#0F1651', '#1C295F', '#743E32', '#170D1A', '#160B0F', '#281516', '#353EA4', '#818DAC', '#E79C3F', '#FACC56', '#3A5DE7', '#B03516',"#5374EE")
 
 load('data/app_data.RData')
